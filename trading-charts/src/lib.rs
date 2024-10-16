50b118fb826ef70c7c11fa48c5f279783845c85c
Frédéric Meyer <frederic.meyer.77@gmail.com>
2024-10-16 20:12:02 +0200

    Formatter le code

--- trading-charts/src/lib.rs
+++ trading-charts/src/lib.rs
@@ -16,10 +16,10 @@ use leptos::{
 
 #[derive(Serialize, Deserialize)]
 struct Candle {
-    time: u64,
-    open: f64,
-    high: f64,
-    low: f64,
+    time:  u64,
+    open:  f64,
+    high:  f64,
+    low:   f64,
     close: f64,
 }
 
@@ -27,7 +27,7 @@ struct Candle {
 struct Background {
     #[serde(rename = "type")]
     r#type: String,
-    color: String,
+    color:  String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,7 +49,7 @@ impl Options {
                 text_color: String::from("black"),
                 background: Background {
                     r#type: String::from("solid"),
-                    color: String::from("white"),
+                    color:  String::from("white"),
                 },
             },
         }
@@ -58,31 +58,91 @@ impl Options {
 
 #[derive(Serialize, Deserialize)]
 struct Series {
-    id: Option<String>,
+    id:      Option<String>,
     #[serde(rename = "type")]
-    r#type: String,
+    r#type:  String,
     options: Option<Options>,
-    data: Vec<Candle>,
+    data:    Vec<Candle>,
 }
 
 impl Series {
     //
     fn new() -> Self {
         Self {
-            id: None,
-            r#type: String::from("candlestick"),
+            id:      None,
+            r#type:  String::from("candlestick"),
             options: None,
-            data: vec![
-                Candle { open: 10., high: 10.63, low: 9.49, close: 9.55, time: 1642427876 },
-                Candle { open: 9.55, high: 10.30, low: 9.42, close: 9.94, time: 1642514276 },
-                Candle { open: 9.94, high: 10.17, low: 9.92, close: 9.78, time: 1642600676 },
-                Candle { open: 9.78, high: 10.59, low: 9.18, close: 9.51, time: 1642687076 },
-                Candle { open: 9.51, high: 10.46, low: 9.10, close: 10.17, time: 1642773476 },
-                Candle { open: 10.17, high: 10.96, low: 10.16, close: 10.47, time: 1642859876 },
-                Candle { open: 10.47, high: 11.39, low: 10.40, close: 10.81, time: 1642946276 },
-                Candle { open: 10.81, high: 11.60, low: 10.30, close: 10.75, time: 1643032676 },
-                Candle { open: 10.75, high: 11.60, low: 10.49, close: 10.93, time: 1643119076 },
-                Candle { open: 10.93, high: 11.53, low: 10.76, close: 10.96, time: 1643205476 },
+            data:    vec![
+                Candle {
+                    open:  10.,
+                    high:  10.63,
+                    low:   9.49,
+                    close: 9.55,
+                    time:  1642427876,
+                },
+                Candle {
+                    open:  9.55,
+                    high:  10.30,
+                    low:   9.42,
+                    close: 9.94,
+                    time:  1642514276,
+                },
+                Candle {
+                    open:  9.94,
+                    high:  10.17,
+                    low:   9.92,
+                    close: 9.78,
+                    time:  1642600676,
+                },
+                Candle {
+                    open:  9.78,
+                    high:  10.59,
+                    low:   9.18,
+                    close: 9.51,
+                    time:  1642687076,
+                },
+                Candle {
+                    open:  9.51,
+                    high:  10.46,
+                    low:   9.10,
+                    close: 10.17,
+                    time:  1642773476,
+                },
+                Candle {
+                    open:  10.17,
+                    high:  10.96,
+                    low:   10.16,
+                    close: 10.47,
+                    time:  1642859876,
+                },
+                Candle {
+                    open:  10.47,
+                    high:  11.39,
+                    low:   10.40,
+                    close: 10.81,
+                    time:  1642946276,
+                },
+                Candle {
+                    open:  10.81,
+                    high:  11.60,
+                    low:   10.30,
+                    close: 10.75,
+                    time:  1643032676,
+                },
+                Candle {
+                    open:  10.75,
+                    high:  11.60,
+                    low:   10.49,
+                    close: 10.93,
+                    time:  1643119076,
+                },
+                Candle {
+                    open:  10.93,
+                    high:  11.53,
+                    low:   10.76,
+                    close: 10.96,
+                    time:  1643205476,
+                },
             ],
         }
     }
@@ -90,8 +150,8 @@ impl Series {
 
 #[derive(Serialize, Deserialize)]
 struct Marker {
-    time: u64,
-    text: String,
+    time:   u64,
+    text:   String,
     #[serde(rename = "type")]
     r#type: String,
 }
@@ -99,9 +159,9 @@ struct Marker {
 impl Marker {
     fn new() -> Self {
         Self {
-            time: 1642773476,
+            time:   1642773476,
             r#type: String::from("buy"),
-            text: String::from("Buy here"),
+            text:   String::from("Buy here"),
         }
     }
 }
