c54fc5a856a30100d9388c89385834a784850b0c
Frédéric Meyer <frederic.meyer.77@gmail.com>
2025-05-28 08:04:35 +0200

    Modification de l'exemple pour mettre du dynamisme dans les données

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -16,8 +16,8 @@ use charts::{
 };
 
 use leptos::{
-    tachys::html::attribute::global::StyleAttribute,
-    reactive::signal::{ReadSignal, RwSignal, signal},
+    tachys::html::attribute::global::{StyleAttribute, OnAttribute},
+    reactive::{signal::RwSignal, traits::{Update, With}, wrappers::read::Signal},
     html::ElementChild,
     IntoView,
     component,
@@ -31,6 +31,35 @@ use std::str::FromStr;
 extern "C" {
     #[wasm_bindgen(js_namespace = Math)]
     fn random() -> f64;
+}
+
+struct Data {
+    data_up: Vec<Candlestick>,
+    data_down: Vec<Candlestick>,
+    markers: Vec<Marker>,
+    counter: usize,
+}
+
+impl Data {
+    fn new() -> Self {
+        let data_up = make_dataset();
+        let data_down = make_dataset();
+        let markers = make_markers(&data_up, 0);
+        let counter = 0;
+
+        Self {
+            data_up,
+            data_down,
+            markers,
+            counter,
+        }
+    }
+    
+    fn inc(&mut self) {
+        self.counter = (self.counter + 1) % 16;
+        self.markers = make_markers(&self.data_down, self.counter);
+        self.data_down = make_dataset();
+    }
 }
 
 fn make_data() -> Result<Vec<Candlestick>, JsError> {
@@ -49,46 +78,61 @@ fn make_data() -> Result<Vec<Candlestick>, JsError> {
     ];
 
     let mut open = 10.55;
-    for i in 12..=30 {
-        let high = open + random() * (12.5 - open);
-        let low = 8.5 + random() * (open - 8.5);
-        let close = low + random() * (high - low);
+    let mut i = 12usize;
+    for m in 4usize..7 {
+        let s = i;
+        i = 1;
 
-        data.push(Candlestick::new(
-            UTCTimestamp::from_str(&format!("2024-04-{:02}", i))?,
-            open,
-            high,
-            low,
-            close,
-        ));
-        open = close;
+        for d in s..=30 {
+            let high = open + random() * (12.5 - open);
+            let low = 8.5 + random() * (open - 8.5);
+            let close = low + random() * (high - low);
+
+            data.push(Candlestick::new(
+                UTCTimestamp::from_str(&format!("2024-{m:02}-{d:02}"))?,
+                open,
+                high,
+                low,
+                close,
+            ));
+            open = close;
+        }
     }
 
     Ok(data)
 }
 
-fn make_markers(data: &Vec<Candlestick>) -> Vec<Marker> {
+fn make_markers(data: &Vec<Candlestick>, delta: usize) -> Vec<Marker> {
     let mut markers = vec![];
 
-    if data.len() > 16 {
-        markers.push(Marker::buy(data[11].time()).with_text(String::from("Yes, buy here")));
-        markers.push(Marker::sell(data[16].time()).with_text(String::from("Sell here, sure ?")));
-    }
+    markers.push(Marker::buy(data[8 + delta].time()).with_text(String::from("B0")));
+    markers.push(Marker::buy(data[11 + delta].time()).with_text(String::from("Yes, buy here")));
+    markers.push(Marker::sell(data[16 + delta].time()).with_text(String::from("Sell here, sure ?")));
+    markers.push(Marker::buy(data[19 + delta].time()).with_text(String::from("B1")));
+    markers.push(Marker::sell(data[23 + delta].time()).with_text(String::from("S1")));
+    markers.push(Marker::sell(data[26 + delta].time()).with_text(String::from("S2")));
+    markers.push(Marker::sell(data[32 + delta].time()).with_text(String::from("S3")));
+    markers.push(Marker::buy(data[35 + delta].time()).with_text(String::from("B2")));
+    markers.push(Marker::buy(data[40 + delta].time()).with_text(String::from("B3")));
+    markers.push(Marker::buy(data[42 + delta].time()).with_text(String::from("B4")));
+    markers.push(Marker::sell(data[47 + delta].time()).with_text(String::from("S4")));
+    markers.push(Marker::buy(data[48 + delta].time()).with_text(String::from("B5")));
+    markers.push(Marker::sell(data[53 + delta].time()).with_text(String::from("S5")));
+    markers.push(Marker::buy(data[60 + delta].time()).with_text(String::from("B6")));
+    markers.push(Marker::buy(data[65 + delta].time()).with_text(String::from("B7")));
+    markers.push(Marker::buy(data[68 + delta].time()).with_text(String::from("B7")));
+    markers.push(Marker::sell(data[70 + delta].time()).with_text(String::from("S6")));
+    markers.push(Marker::sell(data[72 + delta].time()).with_text(String::from("S7")));
 
     markers
 }
 
-fn make_dataset() -> (ReadSignal<Vec<Candlestick>>, ReadSignal<Vec<Marker>>) {
-    let data = make_data().unwrap_or_else(|err| {
+fn make_dataset() -> Vec<Candlestick> {
+    make_data().unwrap_or_else(|err| {
         err.log();
 
         vec![]
-    });
-
-    let (markers, _) = signal(make_markers(&data));
-    let (data, _) = signal(data);
-
-    (data, markers)
+    })
 }
 
 #[component]
@@ -99,21 +143,37 @@ pub fn App() -> impl IntoView {
         .with_panes(LayoutPanesOptions::new());
 
     let options = RwSignal::new(ChartOptions::new().with_layout(layout).with_auto_size(true));
-
-    let (data_up, markers_up) = make_dataset();
-    let (data_down, markers_down) = make_dataset();
+    let data = RwSignal::new(Data::new());
 
     view! {
         <div style="margin-top:10px;padding:10px">
             <div style="border:1px dashed black;height:768px">
                 <Chart options=options.read_only() style="width:100%;height:100%">
                     <ChartPanel>
-                        <CandleStickSeries data=data_up markers=markers_up />
+                        <CandleStickSeries
+                            data=Signal::derive(move || data.with(|d| d.data_up.clone()))
+                            markers=Signal::derive(move || data.with(|d| d.markers.clone()))
+                        />
                     </ChartPanel>
                     <ChartPanel>
-                        <CandleStickSeries data=data_down markers=markers_down />
+                        <CandleStickSeries
+                            data=Signal::derive(move || data.with(|d| d.data_down.clone()))
+                            markers=Signal::derive(move || data.with(|d| d.markers.clone()))
+                        />
                     </ChartPanel>
                 </Chart>
+            </div>
+        
+            <div style="margin-top:10px">
+                <button
+                    on:click=move |_| {
+                        data.update(|d| {
+                            d.inc();
+                        });
+                    }
+                >
+                    "Change markers"
+                </button>
             </div>
         </div>
     }
