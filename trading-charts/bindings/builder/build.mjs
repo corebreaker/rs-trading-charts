306433dccd09e3f3dc478a295b3b2ac4eb277063
Frédéric Meyer <frederic.meyer.77@gmail.com>
2025-06-02 19:45:12 +0200

    Compléter le builder
    On propage la config à `esbuild`.

--- trading-charts/bindings/builder/build.mjs
+++ trading-charts/bindings/builder/build.mjs
@@ -1,18 +1,36 @@
 import esbuild from 'esbuild';
 
+const profile = process.env.PROFILE;
+const node_env = process.env.NODE_ENV || '`undefined`';
+const args = process.argv;
+const minify = args.includes('--minify') || (args.includes('--auto') && profile !== 'debug');
+
 async function main() {
-    await esbuild.build({
-        minify: process.argv.includes('--minify'),
+    const options = {
+        minify,
         entryPoints: ['./src/index.mjs'],
         outfile: './target/module.mjs',
         platform: 'node',
         format: 'esm',
         bundle: true,
-        allowOverwrite: true
-    });
+        allowOverwrite: true,
+    };
+
+    if (!minify) {
+        Object.assign(options, {
+            keepNames: true,
+            conditions: ['development', 'module'],
+            define: {
+                'process.env.DEBUG': JSON.stringify('true'),
+                'process.env.NODE_ENV': JSON.stringify('development')
+            }
+        });
+    }
+
+    await esbuild.build(options);
 }
 
-console.log('Building...');
+console.log(`Building with profile env = '${profile}', node env ='${node_env}', and minify option = ${minify} ...`);
 main()
     .then(() => {console.log('Done.');})
     .catch((err) => {console.error(err);});
